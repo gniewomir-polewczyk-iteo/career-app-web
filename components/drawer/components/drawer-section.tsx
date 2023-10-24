1ccdc39a45bd782fe1cc403ccbf66d0493806aa0
Gniewomir Polewczyk <gniewomir.polewczyk@iteo.com>
2023-10-24 11:39:19 +0200

    Components cleanup

--- components/drawer/components/drawer-section.tsx
+++ components/drawer/components/drawer-section.tsx
@@ -1,40 +1,43 @@
-import React, {useState} from 'react';
-import {Stack, Typography} from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import React, { useState } from "react";
+import { Stack, Typography } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 
 type Props = {
-    title?: string;
-    children: React.ReactNode;
+  title?: string;
+  children: React.ReactNode;
 };
 
-export const DrawerSection: React.FC<Props> = (props) => {
-    const {title, children} = props;
-    const [expanded, setExpanded] = useState(false);
-    const handleToggle = () => {
-        setExpanded(!expanded);
-    };
-    return (
-        <Stack direction="column">
-            <Stack
-                direction="row"
-                alignItems="center"
-                justifyContent="space-between"
-                sx={{p: '12px', cursor: 'pointer'}}
-                onClick={handleToggle}
-            >
-                <Typography
-                    variant="subtitle1"
-                    sx={{color: 'primary.contrastText', textTransform: 'uppercase'}}
-                >
-                    {title}
-                </Typography>
-                {expanded ?
-                    <ExpandLessIcon sx={{color: 'primary.contrastText'}}/> :
-                    <ExpandMoreIcon sx={{color: 'primary.contrastText'}}/>
-                }
-            </Stack>
-            {expanded && children}
-        </Stack>
-    );
+const DrawerSection: React.FC<Props> = (props) => {
+  const { title, children } = props;
+  const [expanded, setExpanded] = useState(false);
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+  return (
+    <Stack direction="column">
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+        sx={{ p: "12px", cursor: "pointer" }}
+        onClick={handleToggle}
+      >
+        <Typography
+          variant="subtitle1"
+          sx={{ color: "primary.contrastText", textTransform: "uppercase" }}
+        >
+          {title}
+        </Typography>
+        {expanded ? (
+          <ExpandLessIcon sx={{ color: "primary.contrastText" }} />
+        ) : (
+          <ExpandMoreIcon sx={{ color: "primary.contrastText" }} />
+        )}
+      </Stack>
+      {expanded && children}
+    </Stack>
+  );
 };
+
+export default DrawerSection;
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,34 +1,33 @@
-import PostType from "../interfaces/post";
+import { Stack } from "@mui/system";
+import Drawer from "./drawer/drawer";
 import Meta from "./meta";
-import React from "react";
-import {Stack} from "@mui/system";
-import {Drawer} from "./drawer/drawer";
+import PostType from "../interfaces/post";
 
 type Props = {
-    children: React.ReactNode;
-    posts: PostType[];
-    preview?: boolean;
+  children: React.ReactNode;
+  posts: PostType[];
+  preview?: boolean;
 };
 
-const Layout = ({children, posts}: Props) => {
-    return (
-        <Stack direction={'row'} minHeight={'100vh'} position={'relative'}>
-            <Meta/>
-            <Drawer posts={posts}/>
-            <Stack
-                direction="column"
-                sx={{
-                    p: 4,
-                    height: '100vh',
-                    width: `calc(100% - 280px)`,
-                    transform: `translateX(280px)`,
-                    transition: 'all 300ms',
-                }}
-            >
-                <main>{children}</main>
-            </Stack>
-        </Stack>
-    );
+const Layout = ({ children, posts }: Props) => {
+  return (
+    <Stack direction={"row"} minHeight={"100vh"} position={"relative"}>
+      <Meta />
+      <Drawer posts={posts} />
+      <Stack
+        direction="column"
+        sx={{
+          p: 4,
+          height: "100vh",
+          width: `calc(100% - 280px)`,
+          transform: `translateX(280px)`,
+          transition: "all 300ms",
+        }}
+      >
+        <main>{children}</main>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Layout;
